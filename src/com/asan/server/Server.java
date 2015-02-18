40db13d8ec069ed1009f2a47db4e2a202dd52e7f
ali amiri <ali_amiri_8131001@yahoo.com>
2015-02-18 14:21:52 +0330

    a

--- src/com/asan/server/Server.java
+++ src/com/asan/server/Server.java
@@ -18,27 +18,23 @@ public class Server {
     }
 
     public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap(); // (2)
+            ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class) // (3)
+                    .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
                         public void initChannel(SocketChannel ch) throws Exception {
                             ch.pipeline().addLast(new ServerHandler());
                         }
                     })
-                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
 
-            // Bind and start to accept incoming connections.
-            ChannelFuture f = b.bind(port).sync(); // (7)
+            ChannelFuture f = b.bind(port).sync();
 
-            // Wait until the server socket is closed.
-            // In this server, this does not happen, but you can do that to gracefully
-            // shut down your server.
             f.channel().closeFuture().sync();
         } finally {
             workerGroup.shutdownGracefully();
--- src/com/asan/server/ServerHandler.java
+++ src/com/asan/server/ServerHandler.java
@@ -4,43 +4,62 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-public class ServerHandler extends ChannelInboundHandlerAdapter { // (1)
+public class ServerHandler extends ChannelInboundHandlerAdapter {
 
-    static int i = 0;
+    private ByteBuf buf;
+
+    static int currentLen = 0;
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-        ByteBuf in = (ByteBuf) msg;
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
 
-        byte[] b = new byte[2];
-        b[0] = in.readByte();
-        b[1] = in.readByte();
-
-        int len = Integer.parseInt(BCDtoString(b));
-
-        byte[] fullMessage = new byte[len];
-
-        int i = 0;
-        fullMessage[i] = b[0];
-        i++;
-        fullMessage[i] = b[1];
-        i++;
-
-        while (in.isReadable()) {
-            fullMessage[i] = in.readByte();
-            i++;
+        if (buf == null) {
+            buf = ctx.alloc().buffer(100000);
         }
+        ByteBuf m = (ByteBuf) msg;
+        buf.writeBytes(m);
+        m.release();
 
-        fullMessage = swapbytes(fullMessage,3,5);
-        fullMessage = swapbytes(fullMessage,4,6);
+        while (true) {
+            if (currentLen == 0 && buf.readableBytes() > 1) {
+                byte[] b = new byte[2];
+                b[0] = buf.readByte();
+                b[1] = buf.readByte();
+                currentLen = Integer.parseInt(BCDtoString(b));
+            }
+            if (buf.readableBytes() >= currentLen && currentLen > 0) {
 
-        ByteBuf out =ctx.alloc().buffer(len);
+                int i = 0;
+                byte[] fullMessage = new byte[currentLen + 2];
+                byte[] len = DecToBCDArray(currentLen, 2);
 
-        out.writeBytes(fullMessage);
-        ctx.writeAndFlush(out);
+                fullMessage[i] = len[0];
+                i++;
+                fullMessage[i] = len[1];
+                i++;
+
+                while (i < fullMessage.length) {
+                    fullMessage[i] = buf.readByte();
+                    i++;
+                }
+
+                fullMessage = swapbytes(fullMessage, 3, 5);
+                fullMessage = swapbytes(fullMessage, 4, 6);
+
+                if (fullMessage[11] == 1) {
+                    ByteBuf out = ctx.alloc().buffer(currentLen);
+
+                    out.writeBytes(fullMessage);
+                    ctx.writeAndFlush(out);
+                }
+                currentLen = 0;
+            }
+            if ((buf.readableBytes() < currentLen) || (currentLen == 0 && buf.readableBytes() < 1) )
+                break;
+        }
     }
 
-    private byte[] swapbytes(byte[] in, int i,int j){
+    private byte[] swapbytes(byte[] in, int i, int j) {
         byte temp = in[i];
         in[i] = in[j];
         in[j] = temp;
@@ -53,7 +72,6 @@ public class ServerHandler extends ChannelInboundHandlerAdapter { // (1)
         cause.printStackTrace();
         ctx.close();
     }
-
 
 
     public static String BCDtoString(byte bcd) {
@@ -77,5 +95,44 @@ public class ServerHandler extends ChannelInboundHandlerAdapter { // (1)
         }
 
         return sb.toString();
+    }
+
+    public static byte[] DecToBCDArray(long num, int byteLen) {
+        int digits = 0;
+
+        long temp = num;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        if (byteLen == -1)
+            byteLen = digits % 2 == 0 ? digits / 2 : (digits + 1) / 2;
+        boolean isOdd = digits % 2 != 0;
+
+        byte bcd[] = new byte[byteLen];
+
+        for (int i = 0; i < digits; i++) {
+            byte tmp = (byte) (num % 10);
+
+            if (i == digits - 1 && isOdd)
+                bcd[i / 2] = tmp;
+            else if (i % 2 == 0)
+                bcd[i / 2] = tmp;
+            else {
+                byte foo = (byte) (tmp << 4);
+                bcd[i / 2] |= foo;
+            }
+
+            num /= 10;
+        }
+
+        for (int i = 0; i < byteLen / 2; i++) {
+            byte tmp = bcd[i];
+            bcd[i] = bcd[byteLen - i - 1];
+            bcd[byteLen - i - 1] = tmp;
+        }
+
+        return bcd;
     }
 }
